8a7d277380350edd0dba09e744f94480f8d5a0fb
D4rK7355608 <d4rk7355608@gmail.com>
2025-11-15 14:39:14 +0200

    nofix

--- src/SmartCleanerForWindows/Shell/App.xaml.cs
+++ src/SmartCleanerForWindows/Shell/App.xaml.cs
@@ -1,19 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
 
 namespace Smart_Cleaner_for_Windows.Shell;
 
-public partial class App
+public partial class App : Application
 {
     private Window? _window;
 
     public App()
     {
         InitializeComponent();
+        AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
     }
 
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         _window = new MainWindow();
         _window.Activate();
+    }
+
+    private void OnUnhandledException(object? sender, global::System.UnhandledExceptionEventArgs e)
+        => LogException(e.ExceptionObject as Exception, "UnhandledException");
+
+    private void OnUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        => LogException(e.Exception, "UnobservedTaskException");
+
+    private static void LogException(Exception? exception, string source)
+    {
+        if (exception is null)
+        {
+            return;
+        }
+
+        try
+        {
+            var directory = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "SmartCleanerForWindows");
+            Directory.CreateDirectory(directory);
+
+            var logPath = Path.Combine(directory, "crash.log");
+            File.AppendAllText(logPath,
+                $"{DateTime.Now:u} [{source}]\r\n{exception}\r\n\r\n");
+        }
+        catch
+        {
+            // Intentionally swallow all exceptions from logging to avoid masking the original failure.
+        }
     }
 }
