a6137e6416996d8116408eb2279b0b952075e391
D4rK7355608 <d4rk7355608@gmail.com>
2025-11-15 15:03:44 +0200

    cccc

--- src/SmartCleanerForWindows/Program.cs
+++ src/SmartCleanerForWindows/Program.cs
@@ -2,22 +2,57 @@ using System;
 using System.Threading;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
+using Microsoft.Windows.ApplicationModel.DynamicDependency;
 using WinRT;
+using Windows.ApplicationModel;
 
 namespace Smart_Cleaner_for_Windows;
 
 public abstract class Program
 {
+    private const uint WindowsAppSdkMajorMinor = 0x00010008;
+
     [STAThread]
     public static void Main(string[] args)
     {
         ComWrappersSupport.InitializeComWrappers();
 
-        Application.Start(p =>
+        var isPackaged = IsRunningPackaged();
+        var bootstrapInitialized = false;
+
+        if (!isPackaged)
         {
-            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
-            SynchronizationContext.SetSynchronizationContext(context);
-            new Shell.App();
-        });
+            Bootstrap.Initialize(WindowsAppSdkMajorMinor);
+            bootstrapInitialized = true;
+        }
+
+        try
+        {
+            Application.Start(p =>
+            {
+                var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                _ = new Shell.App();
+            });
+        }
+        finally
+        {
+            if (bootstrapInitialized)
+            {
+                Bootstrap.Shutdown();
+            }
+        }
+    }
+
+    private static bool IsRunningPackaged()
+    {
+        try
+        {
+            return Package.Current is not null;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
     }
 }
