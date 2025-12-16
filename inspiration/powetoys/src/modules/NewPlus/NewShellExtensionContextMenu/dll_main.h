5b1057bcfca041634411fff89e56fb8a28b17a47
D4rK7355608 <d4rk7355608@gmail.com>
2025-12-16 11:30:22 +0200

    Clean-up code

--- inspiration/powetoys/src/modules/NewPlus/NewShellExtensionContextMenu/dll_main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <common/Telemetry/EtwTrace/EtwTrace.h>
-
-extern HMODULE module_instance_handle;
-extern Shared::Trace::ETWTrace trace;
--- inspiration/powetoys/src/modules/keyboardmanager/KeyboardManagerEditorLibraryWrapper/KeyboardManagerEditorLibraryWrapper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <keyboardmanager/common/Helpers.h>
-#include <keyboardmanager/common/Input.h>
-#include <keyboardmanager/common/MappingConfiguration.h>
-
-extern "C" __declspec(dllexport) bool CheckIfRemappingsAreValid();
